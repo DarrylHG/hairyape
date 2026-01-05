d4db57283420232e75fe36a119276b0de5124b16
Darryl Huang <your-github-email@example.com>
2026-01-05 23:47:47 +0800

    Add responsive mobile navbar

--- src/layout.jsx
+++ src/layout.jsx
@@ -155,13 +155,13 @@ function Layout({ children }) {
             to="/Contact"
             className="block whitespace-nowrap py-2 text-sm md:text-base font-semibold hover:underline px-6"
           >
-            🦍 SIGN UP WITH HARRY THE HAIRY APE NOW TO GET A COMPLIMENTARY STARTER KIT FOR YOUR CHILD! 🎒🏊‍♂️ LIMITED SETS ONLY!!
+            🦍 SIGN UP WITH HARRY THE APE CHAMP NOW TO GET A COMPLIMENTARY STARTER KIT FOR YOUR CHILD! 🎒🏊‍♂️ LIMITED SETS ONLY!!
           </NavLink>
           <NavLink
             to="/Contact"
             className="block whitespace-nowrap py-2 text-sm md:text-base font-semibold hover:underline px-6"
           >
-            🦍 SIGN UP WITH HARRY THE HAIRY APE NOW TO GET A COMPLIMENTARY STARTER KIT FOR YOUR CHILD! 🎒🏊‍♂️ LIMITED SETS ONLY!!
+            🦍 SIGN UP WITH HARRY THE APE CHAMP NOW TO GET A COMPLIMENTARY STARTER KIT FOR YOUR CHILD! 🎒🏊‍♂️ LIMITED SETS ONLY!!
           </NavLink>
         </div>
       </div>
